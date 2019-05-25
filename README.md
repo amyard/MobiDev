41bb0215842d6f91cb909ac1b30d0ca5c4de1150
amyard <maksymturenko@gmail.com>
2019-05-26 00:13:39 +0300

    finish

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # MobiDev
+
+
+    git clone https://github.com/amyard/MobiDev.git
+    cd MobiDev
+    python manage.py makemigrations
+    python manage.py migrate
+    python populate.py
+    python manage.py makemigrations
--- config/settings.py
+++ config/settings.py
@@ -98,14 +98,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mobidev',
-        'USER' : 'delme',
-        'PASSWORD' : 'zaza1234',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'mobidev',
+        # 'USER' : 'delme',
+        # 'PASSWORD' : 'zaza1234',
+        # 'HOST' : '127.0.0.1',
+        # 'PORT' : '5432',
     }
 }
 
